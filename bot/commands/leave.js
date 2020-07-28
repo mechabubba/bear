9accf4863b3d0a8dc4a76f2ec0588199a9463369
06000208 <06000208@users.noreply.github.com>
2020-07-28 13:41:46 -0500

    Tiny spelling change

--- bot/commands/leave.js
+++ bot/commands/leave.js
@@ -15,7 +15,7 @@ module.exports = new CommandBlock({
   if (!content) return message.channel.send(`Arguments required\nUsage: \`${this.firstName} ${this.usage}\``);
   if (!client.guilds.cache.has(content)) return message.channel.send(`${client.user.tag} did not have \`${content}\` mapped to a guild in the guilds cache`);
   const guild = client.guilds.cache.get(content);
-  if (!guild.available) return message.channel.send("Guild was marked as unavailable and thus could not be interacted with. This is indicative of a server outage.");
+  if (!guild.available) return message.channel.send("Guild was marked as unavailable and thus couldn't be interacted with. This is indicative of a server outage.");
   try {
     await guild.leave();
   } catch (error) {
