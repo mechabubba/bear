f96a7a8d5c4be778a28a96c3796863cd46b0194b
06000208 <06000208@users.noreply.github.com>
2020-08-16 20:35:04 -0500

    Enable & disable commands

--- bot/commands/moduleCommands.js
+++ bot/commands/moduleCommands.js
@@ -1,8 +1,8 @@
 const CommandBlock = require("../../modules/CommandBlock");
 const Response = require("../../modules/Response");
 const log = require("../../modules/log");
-const _ = require("lodash");
-const { inspect } = require("util");
+const { forAny } = require("../../modules/miscellaneous");
+const { isArray } = require("lodash");
 const commands = ["c", "cmd", "cmds", "command", "commands"];
 const events = ["e", "event", "events", "l", "listen", "listener", "listeners"];
 const determineConstruct = function(choice) {
@@ -62,7 +62,7 @@ module.exports = [
     if (!constructProperty) return message.channel.send(`Unknown construct "${choice}"\nUsage: \`${this.firstName} ${this.usage}\``);
     const filePath = content.substring(choice.length).trim();
     if (!filePath.length) return message.channel.send(`A path is required\nUsage: \`${this.firstName} ${this.usage}\``);
-    const loadResult = client.handler.requireModule(client[constructProperty], filePath);
+    const loadResult = client.handler.requireModule(client[constructProperty], filePath, false);
     return message.channel.send(`\`\`\`\n${loadResult.message}\n\`\`\``);
   }),
   new CommandBlock({
@@ -80,7 +80,7 @@ module.exports = [
     const constructProperty = determineConstruct(choice);
     if (!constructProperty) return message.channel.send(`Unknown construct "${choice}"\nUsage: \`${this.firstName} ${this.usage}\``);
     const pathsResult = resolveInputToPaths(client, constructProperty, content, choice);
-    const unloadResult = _.isArray(pathsResult.value) ? client.handler.unloadMultipleModules(client[constructProperty], pathsResult.value) : client.handler.unloadModule(client[constructProperty], pathsResult.value);
+    const unloadResult = isArray(pathsResult.value) ? client.handler.unloadMultipleModules(client[constructProperty], pathsResult.value) : client.handler.unloadModule(client[constructProperty], pathsResult.value);
     return message.channel.send(`\`\`\`\n${pathsResult.message}\n${unloadResult.message}\n\`\`\``);
   }),
   new CommandBlock({
@@ -99,9 +99,59 @@ module.exports = [
     if (!constructProperty) return message.channel.send(`Unknown construct "${choice}"\nUsage: \`${this.firstName} ${this.usage}\``);
     const pathsResult = resolveInputToPaths(client, constructProperty, content, choice);
     if (!pathsResult.value) return message.channel.send(`A path or name is required\nIf targeting anonymous blocks, use \`unload\` instead\nUsage: \`${this.firstName} ${this.usage}\``);
-    const unloadResult = _.isArray(pathsResult.value) ? client.handler.unloadMultipleModules(client[constructProperty], pathsResult.value) : client.handler.unloadModule(client[constructProperty], pathsResult.value);
+    const unloadResult = isArray(pathsResult.value) ? client.handler.unloadMultipleModules(client[constructProperty], pathsResult.value) : client.handler.unloadModule(client[constructProperty], pathsResult.value);
     if (!unloadResult.success || unloadResult.error) return message.channel.send(`\`\`\`\n${pathsResult.message}\n${unloadResult.message}\n\`\`\``);
-    const loadResult = _.isArray(pathsResult.value) ? client.handler.requireMultipleModules(client[constructProperty], false, pathsResult.value) : client.handler.requireModule(client[constructProperty], pathsResult.value);
+    const loadResult = isArray(pathsResult.value) ? client.handler.requireMultipleModules(client[constructProperty], pathsResult.value, false) : client.handler.requireModule(client[constructProperty], pathsResult.value, false);
     return message.channel.send(`\`\`\`\n${pathsResult.message}\n${unloadResult.message}\n${loadResult.message}\n\`\`\``);
+  }),
+  new CommandBlock({
+    identity: "enable",
+    summary: "Enable modules by path",
+    description: "Enable command or event modules by file path. Note that the /modules/ folder is treated as the working directory.",
+    usage: "command/event <path>",
+    scope: ["dm", "text", "news"],
+    nsfw: false,
+    locked: "hosts",
+    clientPermissions: ["VIEW_CHANNEL", "SEND_MESSAGES"],
+    userPermissions: null,
+  }, function(client, message, content, [choice, args]) {
+    if (!choice) return message.channel.send(`Usage: \`${this.firstName} ${this.usage}\``);
+    const constructProperty = determineConstruct(choice);
+    if (!constructProperty) return message.channel.send(`Unknown construct "${choice}"\nUsage: \`${this.firstName} ${this.usage}\``);
+    const filePath = content.substring(choice.length).trim();
+    if (!filePath.length) return message.channel.send(`A path is required\nUsage: \`${this.firstName} ${this.usage}\``);
+    const loadResult = client.handler.requireModule(client[constructProperty], filePath, false);
+    // you have to put the path in an array so that periods aren't interpreted as traversing the db
+    if (loadResult.value) client.handler.modules.set([loadResult.value], true).write();
+    return message.channel.send(`\`\`\`\n${loadResult.message}\n${loadResult.value ? "Enabled the module in modules.json" : ""}\`\`\``);
+  }),
+  new CommandBlock({
+    identity: "disable",
+    summary: "Disable modules by name/path",
+    description: "Disable command or event modules by command name, event name, or file path. Note that the /modules/ folder is treated as the working directory. Be careful about which modules you disable and ",
+    usage: "command/event <name/path>",
+    scope: ["dm", "text", "news"],
+    nsfw: false,
+    locked: "hosts",
+    clientPermissions: ["VIEW_CHANNEL", "SEND_MESSAGES"],
+    userPermissions: null,
+  }, function(client, message, content, [choice, args]) {
+    if (!choice) return message.channel.send(`Usage: \`${this.firstName} ${this.usage}\``);
+    const constructProperty = determineConstruct(choice);
+    if (!constructProperty) return message.channel.send(`Unknown construct "${choice}"\nUsage: \`${this.firstName} ${this.usage}\``);
+    const pathsResult = resolveInputToPaths(client, constructProperty, content, choice);
+    const multipleModules = isArray(pathsResult.value);
+    const unloadResult = multipleModules ? client.handler.unloadMultipleModules(client[constructProperty], pathsResult.value) : client.handler.unloadModule(client[constructProperty], pathsResult.value);
+    if (unloadResult.value) {
+      // you have to put the path in an array so that periods aren't interpreted as traversing the db
+      if (multipleModules) {
+        for (const resolvedPath of unloadResult.value) {
+          client.handler.modules.set([resolvedPath], false).write();
+        }
+      } else {
+        client.handler.modules.set([unloadResult.value], false).write();
+      }
+    }
+    return message.channel.send(`\`\`\`\n${pathsResult.message}\n${unloadResult.message}\n${unloadResult.value ? `Disabled the ${multipleModules ? "modules" : "module"} in modules.json` : ""}\`\`\``);
   }),
 ];
