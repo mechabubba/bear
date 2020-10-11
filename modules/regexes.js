bbad1e257ee4f4317fb56724ce727f8a33070603
06000208 <06000208@users.noreply.github.com>
2020-10-11 04:58:38 -0500

    Update regexes.js

--- modules/regexes.js
+++ modules/regexes.js
@@ -4,16 +4,21 @@
  */
 
 /**
- *
+ * Matches only characters in the ranges A-Z and a-z from the beginning to the end of the string.
  */
-module.exports.alphabetic = /^\d+$/;
+module.exports.alphabetic = /^[A-Za-z]+$/;
 
 /**
- *
+ * Matches only digit characters (0-9) from the beginning to the end of the string.
  */
 module.exports.numeric = /^\d+$/;
 
 /**
- *
+ * Matches only characters in the ranges A-Z, a-z, and any digit character (0-9) from the beginning to the end of the string.
  */
 module.exports.alphanumeric = /^[A-Za-z\d]+$/;
+
+/**
+ * Matches the format for a discord authentication token from the beginning to the end of the string.
+ */
+module.exports.token = /^[\w]{24}\.[\w-]{6}\.[\w-]{27}$/;
