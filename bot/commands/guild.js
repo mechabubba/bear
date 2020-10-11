ec40ef005e2ae7e50e7bd9945abe5bb2099d56be
06000208 <06000208@users.noreply.github.com>
2020-10-11 13:13:54 -0500

    Respect guild privacy when widget is disabled

--- bot/commands/guild.js
+++ bot/commands/guild.js
@@ -3,6 +3,8 @@ const log = require("../../modules/log");
 const { numeric } = require("../../modules/regexes");
 const { MessageEmbed } = require("discord.js");
 const chalk = require("chalk");
+const fetch = require("node-fetch");
+const { has } = require("lodash");
 
 module.exports = new CommandBlock({
   identity: ["guild", "guilds"],
@@ -30,20 +32,33 @@ module.exports = new CommandBlock({
   } else {
     if (!numeric.test(id)) return message.channel.send(`The id \`${id}\` was invalid`);
     if (!client.guilds.cache.has(id)) return message.channel.send(`The id \`${id}\` isn't mapped to a guild in the cache`);
-    const guild = client.guilds.cache.get(id);
+    let guild = client.guilds.cache.get(id);
+    guild = await guild.fetch();
     if (!guild.available) return message.channel.send("The guild was unavailable and could not be interacted with. This is indicative of a server outage.");
     const embed = new MessageEmbed()
       .setTitle(guild.name)
-      .setThumbnail(guild.iconURL({ format: "png" }))
       .addFields(
+        { name: "Link", value: `[Jump](https://discordapp.com/channels/${guild.id}/)`, inline: true },
         { name: "Owner", value: guild.owner.user, inline: true },
-        { name: "Members", value: guild.memberCount, inline: true },
-        { name: "Region", value: `\`${guild.region}\``, inline: true },
       )
       .setFooter(guild.id)
       .setTimestamp(guild.createdTimestamp);
     const color = client.config.get("metadata.color").value();
     if (color) embed.setColor(color);
-    return message.channel.send(`<https://discordapp.com/channels/${guild.id}/>`, embed);
+    // Respect guild privacy when widget is disabled
+    // Checked using node-fetch so the Manage Server permission isn't required
+    const response = await fetch(`https://discordapp.com/api/guilds/${guild.id}/widget.json`);
+    const json = await response.json();
+    if (!has(json, "code")) {
+      embed.addFields(
+        { name: "Members", value: guild.memberCount, inline: true },
+        { name: "Region", value: `\`${guild.region}\``, inline: true },
+        { name: "Widget", value: "Enabled", inline: true },
+      ).setThumbnail(guild.iconURL({ format: "png" }));
+    } else {
+      log.warn(`${message.author.tag} requested widget.json for "${guild.id}"`, json);
+      embed.addField("Widget", "Disabled", true);
+    }
+    return message.channel.send(embed);
   }
 });
