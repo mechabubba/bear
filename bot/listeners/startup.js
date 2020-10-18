037ea708857ff444a834f3ef2cbfd2fc8ed93e24
06000208 <06000208@users.noreply.github.com>
2020-10-18 06:59:36 -0500

    Update startup.js

--- bot/listeners/startup.js
+++ bot/listeners/startup.js
@@ -10,13 +10,15 @@ module.exports = new ListenerBlock({
   // But it will only run once, so it's safe to use for things such as scheduling tasks or other one time operations
 
   // Add bot owner to hosts user group
+  // If the users.hosts group is an empty array, this won't happen.
   if (client.config.get("users.hosts").value() === null) {
     const application = await client.fetchApplication();
     // This supports teams, but only the team's owner.
     // If anyone wants to implement real support for team members, it would be appreciated.
     const owner = has(application, "owner.members") ? application.owner.ownerID : application.owner.id;
     client.config.set("users.hosts", [owner]).write();
+    log.info(`Added the bot's owner "${owner}" to the hosts user group.`);
   }
 
-  log.info(`App is now fully functional`);
+  log.info("App is now fully functional");
 });
