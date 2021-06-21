1be24fa9bb896c8e92763fbca1bf0a135545bffa
06000208 <52764066+06000208@users.noreply.github.com>
2021-06-21 09:25:25 -0500

    5 new custom events

--- bot/listeners/customEvents.js
+++ bot/listeners/customEvents.js
@@ -6,7 +6,13 @@ const log = require("../../modules/log");
 const chalk = require("chalk");
 
 module.exports = [
-  // Guild Access Control
+  // Guild Access Control (from modules/GuildManager.js)
   new ListenerBlock({ event: "blockedGuild" }, (client, guild) => log.debug(`${chalk.gray("[blockedGuild]")} ${client.user.tag} automatically left ${guild.name} (${guild.id})`)),
   new ListenerBlock({ event: "unknownGuild" }, (client, guild) => log.debug(`${chalk.gray("[unknownGuild]")} ${client.user.tag} automatically left ${guild.name} (${guild.id})`)),
+  // Commands (from modules/CommandConstruct.js)
+  new ListenerBlock({ event: "commandUsed" }, (client, command, message, content, args, ...extraParameters) => log.debug(`${chalk.gray("[command]")} ${message.author.tag} ran "${command.firstName}${(!content ? "\"" : `" with "${content}"`)}`)),
+  new ListenerBlock({ event: "channelTypeRejection" }, (client, command, message) => log.debug(`${chalk.gray("[command]")} ${message.author.tag} attempted to run "${command.firstName}" somewhere it cannot be used (${message.channel.type})`)),
+  new ListenerBlock({ event: "nsfwRejection" }, (client, command, message) => log.debug(`${chalk.gray("[command]")} ${message.author.tag} attempted to run "${command.firstName}" in a non-nsfw channel`)),
+  new ListenerBlock({ event: "lockedRejection" }, (client, command, message) => log.debug(`${chalk.gray("[command]")} ${message.author.tag} attempted to run "${command.firstName}" and was denied`)),
+  new ListenerBlock({ event: "permissionRejection" }, (client, command, message, member, permissions) => log.debug(`${chalk.gray("[command]")} ${member.user.tag} lacked permissions necessary to run "${command.firstName}"${(client.user.id === member.user.id ? ` for ${message.author.tag}` : "")}`)),
 ];
