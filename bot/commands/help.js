eec8944b6f315ef512f5afb9d133714b6ac1cacb
06000208 <52764066+06000208@users.noreply.github.com>
2021-06-21 09:25:39 -0500

    Update help.js

--- bot/commands/help.js
+++ bot/commands/help.js
@@ -1,36 +1,10 @@
 const CommandBlock = require("../../modules/CommandBlock");
 const { MessageEmbed } = require("discord.js");
 const log = require("../../modules/log");
-const { isString, isArray } = require("lodash");
 
-const validator = function(client, message, command) {
-  // permissions
-  if (message.channel.type !== "dm") {
-    if (command.userPermissions) {
-      if (!message.member.hasPermission(command.userPermissions, false, true, true)) return false;
-    }
-  }
-  // access control
-  if (command.locked) {
-    if (command.locked === true) return;
-    if (isString(command.locked)) {
-      if (command.locked !== message.author.id) {
-        if (!client.config.has(["users", command.locked]).value()) return false;
-        if (client.config.isNil(["users", command.locked]).value()) return false;
-        if (!client.config.get(["users", command.locked]).includes(message.author.id).value()) return false;
-      }
-    } else if (isArray(command.locked)) {
-      if (!command.locked.includes(message.author.id)) {
-        if (command.locked.some((group) => {
-          if (!client.config.has(["users", group]).value()) return false;
-          if (client.config.isNil(["users", group]).value()) return false;
-          if (!client.config.get(["users", group]).includes(message.author.id).value()) return false;
-          return true;
-        }) === false) return false;
-      }
-    }
-  }
-  // passed above code without returning
+const commandPredicate = function(message, command) {
+  if (!command.checkPermissions(message, message.member, command.userPermissions)) return false;
+  if (!command.checkLocked(message)) return false;
   return true;
 };
 
@@ -42,7 +16,7 @@ module.exports = new CommandBlock({
   clientPermissions: ["VIEW_CHANNEL", "SEND_MESSAGES"],
 }, function(client, message, content, args) {
   if (!content) {
-    const commands = client.commands.cache.filter(command => validator(client, message, command));
+    const commands = client.commands.cache.filter(command => commandPredicate(message, command));
     const names = commands.map(command => command.firstName);
     const text = `🔍 To query command info, use \`${this.firstName} ${this.usage}\`\n\`\`\`\n${names.join(", ")}\n\`\`\``;
     if (text.length > 1900) return log.warn("[help] The command list has exceeded 1990 characters in length and is no longer usable!");
@@ -61,7 +35,7 @@ module.exports = new CommandBlock({
       return message.channel.send(`Command \`${content}\` not found`);
     }
     const command = client.commands.cache.get(id);
-    if (!validator(client, message, command)) return message.channel.send(`Command \`${content}\` not found`);
+    if (!commandPredicate(message, command)) return message.channel.send(`Command \`${content}\` not found`);
     const embed = new MessageEmbed()
       .setTitle(command.firstName)
       .setDescription(command.description || command.summary || "No description provided")
