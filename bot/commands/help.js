ca4c7cb8ca00dcdb6c31f31c36c70a6cd7c9058d
06000208 <52764066+06000208@users.noreply.github.com>
2021-06-28 17:53:54 -0500

    Update & improve help command

--- bot/commands/help.js
+++ bot/commands/help.js
@@ -1,9 +1,10 @@
 const CommandBlock = require("../../modules/CommandBlock");
 const { MessageEmbed } = require("discord.js");
 const log = require("../../modules/log");
+const { isArray } = require("lodash");
 
 const commandPredicate = function(message, command) {
-  if (!command.checkPermissions(message, message.member, command.userPermissions)) return false;
+  if (!command.checkPermissions(message, command.userPermissions, false, false)) return false;
   if (!command.checkLocked(message)) return false;
   return true;
 };
@@ -13,13 +14,15 @@ module.exports = new CommandBlock({
   summary: "Lists commands & provides command info",
   description: "Provides a list of commands or info about individual commands when queried.",
   usage: "[command]",
-  clientPermissions: ["VIEW_CHANNEL", "SEND_MESSAGES"],
+  clientChannelPermissions: ["VIEW_CHANNEL", "SEND_MESSAGES"],
 }, function(client, message, content, args) {
   if (!content) {
+    /** @type {Collection<Snowflake, CommandBlock>} */
     const commands = client.commands.cache.filter(command => commandPredicate(message, command));
+    /** @type {Array<String>} */
     const names = commands.map(command => command.firstName);
     const text = `🔍 To query command info, use \`${this.firstName} ${this.usage}\`\n\`\`\`\n${names.join(", ")}\n\`\`\``;
-    if (text.length > 1900) return log.warn("[help] The command list has exceeded 1990 characters in length and is no longer usable!");
+    if (text.length > 1900) return log.warn("[help] The command list has exceeded 1900 characters in length and is no longer usable!");
     const embed = new MessageEmbed()
       .setTitle("Command List")
       .setDescription(text);
@@ -34,15 +37,19 @@ module.exports = new CommandBlock({
       log.warn(`Command name "${name}" was mapped in command index but corresponding id "${id}" isn't mapped in command cache`);
       return message.channel.send(`Command \`${content}\` not found`);
     }
+    /** @type {CommandBlock} */
     const command = client.commands.cache.get(id);
     if (!commandPredicate(message, command)) return message.channel.send(`Command \`${content}\` not found`);
     const embed = new MessageEmbed()
       .setTitle(command.firstName)
       .setDescription(command.description || command.summary || "No description provided")
       .addField("Usage", `\`${command.firstName}${command.usage ? " " + command.usage : ""}\``, true);
+    if (!command.channelTypes.includes("dm")) embed.addField("Direct Messages", "Disallowed", true);
+    if (!command.channelTypes.includes("text")) embed.addField("Guilds", "Disallowed", true);
+    if (command.nsfw) embed.addField("NSFW", "True", true);
+    if (isArray(command.identity) && command.identity.length > 1) embed.setFooter(command.identity.slice(1).join(", "));
     const color = client.config.get("metadata.color").value();
     if (color) embed.setColor(color);
-    if (command.nsfw) embed.addField("NSFW", "True", true);
     return message.channel.send(embed);
   }
 });
