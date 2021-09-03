87eda12741ca3a542708da26b8a81391fd27bca6
mechabubba <minecraftguy66@gmail.com>
2021-09-02 23:10:51 -0500

    minesweeper, anger, and space widths

--- bot/commands/minesweeper.js
+++ bot/commands/minesweeper.js
@@ -1,7 +1,20 @@
 const CommandBlock = require("../../modules/CommandBlock");
 const { MessageEmbed } = require("discord.js");
 const seedrandom = require("seedrandom");
-const mineemoji = "\uD83D\uDCA5";
+
+const tiles = [
+  "．", // Ideographic Space
+  "１",
+  "２",
+  "３",
+  "４",
+  "５",
+  "６",
+  "７",
+  "８", 
+  "＠"  // Mine
+];
+const thepeoplewhodevelopdiscordarefuckingstupid = 199; // The limit to how many mines can be viewed on the Discord client app.
 
 module.exports = new CommandBlock({
     identity: ["minesweeper", "ms", "minesweep"],
@@ -10,7 +23,7 @@ module.exports = new CommandBlock({
     usage: "[length] [width] [mines] (-seed [...values])",
     scope: ["dm", "text", "news"],
     clientPermissions: ["VIEW_CHANNEL", "SEND_MESSAGES"]
-  }, function(client, message, content, [length, width, maxmines, hasseed, ...args]) {
+  }, async function(client, message, content, [length, width, maxmines, hasseed, ...args]) {
     const positive = client.config.get("metadata.reactions.positive").value();
     const negative = client.config.get("metadata.reactions.negative").value();
 
@@ -29,19 +42,20 @@ module.exports = new CommandBlock({
     } else if(area == 0) {
       message.react(negative);
       return message.reply(`<:_:${negative}> You can't create an empty board!`);
-    } else if(area > 198) {
+    }
+    else if(area > thepeoplewhodevelopdiscordarefuckingstupid) {
       message.react(negative);
-      return message.reply(`<:_:${negative}> The board exceeds a maximum of 198 cells.`);
+      return message.reply(`<:_:${negative}> The board exceeds a maximum of ${thepeoplewhodevelopdiscordarefuckingstupid} cells.`);
     }
 
-    /* 
     // Embed descriptions are only able to hold 2048 characters.
     // Minesweeper cells are at most 7 characters (4 for the spoiler bars, at most 3 for the numbers - mines are 2 characters).
     // Newlines take up one character in themselves.
 
     // I *would* use this, however Discord messages limits the message to *display* only the first 198 emotes; the content is sent, and can be viewed in the "Edit message" box or if you're grabbing the raw content of the message via the API, however everything after 198 emotes does not display.
+    /*
     let charea = (area * 7) + (length - 1);
-    if(charea > 2048) {
+    if(charea > (2000 - suffix.length)) {
       message.react(negative);
       return message.reply(`<:_:${negative}> The board exceeds a maximum of 2048 characters.`);
     }
@@ -56,10 +70,10 @@ module.exports = new CommandBlock({
     while(curmines < maxmines) {
       let rl = Math.floor(rng() * length);
       let rw = Math.floor(rng() * width);
-      if(board[rl][rw] == mineemoji) {
+      if(board[rl][rw] == tiles[9]) {
         continue;
       } else {
-        board[rl][rw] = mineemoji;
+        board[rl][rw] = tiles[9];
         curmines++;
       }
     }
@@ -67,16 +81,16 @@ module.exports = new CommandBlock({
     for(i = 0; i < board.length; i++) {
       for(j = 0; j < board[i].length; j++) {
         let mines = 0;
-        if(board[i][j] == mineemoji) continue;
-        if((j - 1 != -1) && board[i][j-1] == mineemoji) mines++; // left
-        if((j - 1 != -1) && (i - 1 != -1) && board[i-1][j-1] == mineemoji) mines++;  // top left
-        if((i - 1 != -1) && board[i-1][j] == mineemoji) mines++; // top
-        if((i - 1 != -1) && (j + 1 != board[i].length) && board[i-1][j+1] == mineemoji) mines++; // top right
-        if((j + 1 != board[i].length) && board[i][j+1] == mineemoji) mines++; // right
-        if((j + 1 != board[i].length) && (i + 1 != board.length) && board[i+1][j+1] == mineemoji) mines++; // bottom right
-        if((i + 1 != board.length) && board[i+1][j] == mineemoji) mines++; // bottom
-        if((i + 1 != board.length) && (j - 1 != -1) && board[i+1][j-1] == mineemoji) mines++; // bottom left
-        board[i][j] = mines + "\u20E3";
+        if(board[i][j] === tiles[9]) continue;
+        if((j - 1 != -1) && board[i][j-1] == tiles[9]) mines++; // left
+        if((j - 1 != -1) && (i - 1 != -1) && board[i-1][j-1] == tiles[9]) mines++;  // top left
+        if((i - 1 != -1) && board[i-1][j] == tiles[9]) mines++; // top
+        if((i - 1 != -1) && (j + 1 != board[i].length) && board[i-1][j+1] == tiles[9]) mines++; // top right
+        if((j + 1 != board[i].length) && board[i][j+1] == tiles[9]) mines++; // right
+        if((j + 1 != board[i].length) && (i + 1 != board.length) && board[i+1][j+1] == tiles[9]) mines++; // bottom right
+        if((i + 1 != board.length) && board[i+1][j] == tiles[9]) mines++; // bottom
+        if((i + 1 != board.length) && (j - 1 != -1) && board[i+1][j-1] == tiles[9]) mines++; // bottom left
+        board[i][j] = tiles[mines];
       }
     }
     
