f3f828e666324f31cee3688c48eac028db09a0e5
mechabubba <stevenvanniisprettycool@gmail.com>
2021-10-01 20:15:16 -0500

    actually add everything

--- modules/log.js
+++ modules/log.js
@@ -1,52 +1,60 @@
+/**
+ * Modified logging module from sandplate. [Original source code](https://github.com/06000208/sandplate/blob/main/modules/log.js)
+ * @module logger
+ * @example
+ * const log = require("./util/logger");
+ */
 const chalk = require("chalk");
-
-// Label names
-const labels = {
-  0: "fatal",
-  1: "error",
-  2: " warn",
-  3: " info",
-  4: "debug",
-  5: "trace",
+const { DateTime } = require("luxon");
+const color = {
+    "fatal": chalk.bgRed.black,
+    "error": chalk.red,
+    " warn": chalk.yellow,
+    " info": chalk.white.bold,
+    " http": chalk.blue,
+    "debug": chalk.green,
+    "trace": chalk.gray,
 };
+const errors = ["fatal", "error"];
+const timestamp = "HH:mm:ss.SSS";
 
-// Label styles
-const styles = {
-  0: chalk.bgRed.black,
-  1: chalk.red,
-  2: chalk.yellow,
-  3: chalk.white.bold,
-  4: chalk.green,
-  5: chalk.gray,
-};
+/**
+ * Logger's timestamp format
+ * @readonly
+ * @todo If it was possible to store this in `.env` that would be nice
+ */
+module.exports.timestamp = timestamp;
 
-// If you would prefer accurate stack tracing when using
-// debuggers like visual studio code's over having timestamps,
-// you can switch by swapping which approach is commented out
-
-// Wrapper function approach
-
-const moment = require("moment");
+/**
+ * Internal function used for logging
+ * @param {*} level
+ * @param  {...any} args
+ * @private
+ */
 const print = function(level, ...args) {
-  const prefix = `${chalk.gray(moment().format("HH:mm:ss.SSS"))} ${styles[level](labels[level])}`;
-  return level > 1 ? console.log(prefix, ...args) : console.error(prefix, ...args);
+    const prefix = `${chalk.gray(DateTime.now().toFormat(timestamp))} ${color[level](level)}`;
+    return errors.includes(level) ? console.error(prefix, ...args) : console.log(prefix, ...args);
 };
-module.exports = (...args) => print(3, ...args);
-module.exports.trace = (...args) => print(5, ...args);
-module.exports.debug = (...args) => print(4, ...args);
-module.exports.info = (...args) => print(3, ...args);
-module.exports.warn = (...args) => print(2, ...args);
-module.exports.error = (...args) => print(1, ...args);
-module.exports.fatal = (...args) => print(0, ...args);
 
-// Bind approach (no timestamps)
-/*
-const prefix = (id) => styles[id](labels[id]);
-module.exports = console.log.bind(console, prefix(3));
-module.exports.trace = console.log.bind(console, prefix(5));
-module.exports.debug = console.log.bind(console, prefix(4));
-module.exports.info = console.log.bind(console, prefix(3));
-module.exports.warn = console.log.bind(console, prefix(2));
-module.exports.error = console.error.bind(console, prefix(1));
-module.exports.fatal = console.error.bind(console, prefix(0));
-*/
+/**
+ * Default exported logging function
+ *
+ * Using `log()` by itself is an alias to `log.info()`
+ * @param  {...any} args
+ * @function log
+ * @example
+ * log("example")
+ * log.fatal("example");
+ * log.error("example");
+ * log.warn("example");
+ * log.info("example");
+ * log.debug("example");
+ * log.trace("example");
+ */
+module.exports = (...args) => print(" info", ...args);
+module.exports.fatal = (...args) => print("fatal", ...args);
+module.exports.error = (...args) => print("error", ...args);
+module.exports.warn = (...args) => print(" warn", ...args);
+module.exports.info = (...args) => print(" info", ...args);
+module.exports.debug = (...args) => print("debug", ...args);
+module.exports.trace = (...args) => print("trace", ...args);
