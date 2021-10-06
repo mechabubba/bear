1c3461af6969605a03ed8f46c105ce3365a567cb
mechabubba <stevenvanniisprettycool@gmail.com>
2021-10-06 18:51:58 -0500

    reminders are fixed.

--- bot/reminderCall.js
+++ bot/reminderCall.js
@@ -1,41 +1,43 @@
 const ListenerBlock = require("../modules/ListenerBlock");
-const { DateTime } = require("luxon");
-
-// Default DateTime format. Used in the reminder announcement.
-const dt_format = DateTime.DATETIME_FULL;
-
-module.exports = new ListenerBlock({ event: "reminderCall" }, ({ client }, reminder) => {
+module.exports = new ListenerBlock({ event: "reminderCall" }, ({ client }, reminder, cronremove) => {
     const reminderalert = `<:_:${client.config.get("metadata.reactions.reminderalert").value()}>`;
+    const alert = `${reminderalert} <@${reminder.userID}>, you set a reminder on **<t:${Math.round(reminder.start.getTime() / 1000)}:f>**;\n${reminder.message}`;
 
-    const start = DateTime.fromJSDate(reminder.start).toLocaleString(dt_format);
-    const alert = `${reminderalert} <@${reminder.userID}>, you set a reminder on **${start}**;\n"${reminder.message}"`;
+    const stop = (uid, rid) => client.reminders.stop(uid, rid);
+
+    const user = client.users.resolve(reminder.userID);
+    if(user === null) {
+        stop(reminder.userID, reminder.id);
+    }
 
     if(reminder.isDM) {
-        if(!client.users.cache.has(reminder.userID)) return client.reminders.stop(reminder.id);
-
-        const user = client.users.cache.get(reminder.userID);
-        return user.send({
+        user.send({
             content: alert,
             allowedMentions: {
                 parse: ["users"],
-                users: [reminder.userID]
             },
         });
     } else {
-        if(!client.guilds.cache.has(reminder.guildID)) return client.reminders.stop(reminder.id);
+        const guild = client.guilds.resolve(reminder.guildID);
+        if(guild !== null) {
+            // We know the user exists, but we need to check if they're a member of the current server aswell.
+            // If not, we have no need to send the message so we can dump it.
+            const member = guild.members.resolve(reminder.userID);
+            if(member !== null) {
+                const channel = guild.channels.resolve(reminder.channelID);
+                if(channel !== null) {
+                    channel.send({
+                        content: alert,
+                        allowedMentions: {
+                            parse: ["users"],
+                        },
+                    });
+                }
+            }
+        }
+    }
 
-        const guild = client.guilds.cache.get(reminder.guildID);
-        if(!guild.members.cache.has(reminder.userID)) return client.reminders.stop(reminder.id);
-        if(!guild.channels.cache.has(reminder.channelID)) return client.reminders.stop(reminder.id);
-
-        const channel = client.guilds.cache.get(reminder.guildID).channels.cache.get(reminder.channelID);
-
-        return channel.send({
-            content: alert,
-            allowedMentions: {
-                parse: ["users"],
-                users: [reminder.userID]
-            },
-        });
+    if(!reminder.isCron || (reminder.isCron && cronremove)) {
+        stop(reminder.userID, reminder.id);
     }
 });
