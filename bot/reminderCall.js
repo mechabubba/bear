d13717fbdf71889057d1f0707ef3210c43e482e0
mechabubba <stevenvanniisprettycool@gmail.com>
2022-10-27 15:04:59 -0500

    fix: reminderCall no longer entirely relying on cache

--- bot/reminderCall.js
+++ bot/reminderCall.js
@@ -1,38 +1,43 @@
 const ListenerBlock = require("../modules/ListenerBlock");
-module.exports = new ListenerBlock({ event: "reminderCall" }, ({ client }, reminder, cronremove) => {
-    const reminderalert = `<:_:${client.config.get("metadata.reactions.reminderalert").value()}>`;
-    const alert = `${reminderalert} <@${reminder.userID}>, you set a reminder on **<t:${reminder.startSecs}:f>**;\n${reminder.message}`;
-    const user = client.users.resolve(reminder.userID);
+module.exports = new ListenerBlock({ event: "reminderCall" }, async ({ client }, reminder, cronremove) => {
     const stop = (uid, rid) => client.reminders.stop(uid, rid);
     
-    if(user === null) {
-        stop(reminder.userID, reminder.id);
-    }
+    let threw = false;
+    try {
+        if(reminder.isDM) {
+            const user = await client.users.fetch(reminder.userID);
+            if(!user) throw new Error("User not found.");
 
-    if(reminder.isDM) {
-        user.send({
-            content: alert,
-            allowedMentions: { parse: ["users"] },
-        });
-    } else {
-        const guild = client.guilds.resolve(reminder.guildID);
-        if(guild !== null) {
-            // We know the user exists, but we need to check if they're a member of the current server aswell.
-            // If not, we have no need to send the message so we can dump it.
-            const member = guild.members.resolve(reminder.userID);
-            if(member !== null) {
-                const channel = guild.channels.resolve(reminder.channelID);
-                if(channel !== null) {
-                    channel.send({
-                        content: alert,
-                        allowedMentions: { parse: ["users"] },
-                    });
-                }
-            }
+            user.send({
+                content: `${reminderalert} <@${reminder.userID}>, you set a reminder on **<t:${reminder.startSecs}:f>**;\n${reminder.message}`,
+                allowedMentions: { parse: ["users"] },
+            });
+        } else {
+            const guild = await client.guilds.fetch(reminder.guildID);
+            if(!guild) throw new Error("Guild not found.");
+
+            const channel = await guild.channels.fetch(reminder.channelID);
+            if(!channel) throw new Error("Channel not found.");
+
+            const user = guild.members.fetch(reminder.userID);
+            if(!user) throw new Error("User is no longer in this guild.");
+
+            channel.send({
+                content: `${reminderalert} <@${reminder.userID}>, you set a reminder on **<t:${reminder.startSecs}:f>**;\n${reminder.message}`,
+                allowedMentions: { parse: ["users"] },
+            });
         }
-    }
-
-    if(!reminder.isCron || (reminder.isCron && cronremove)) {
-        stop(reminder.userID, reminder.id);
+    } catch(e) {
+        // A few possible things happened here;
+        // - The bot could not fetch the guild.
+        // - The bot could not fetch the guild member (or user, in the case of DM's).
+        // - The bot could not fetch the channel.
+        // - The bot could not DM the user.
+        // Whatever the case, it wasn't supposed to happen. We kill the reminder here.
+        threw = true;
+    } finally {
+        if(!reminder.isCron || (reminder.isCron && cronremove) || threw) {
+            stop(reminder.userID, reminder.id);
+        }
     }
 });
