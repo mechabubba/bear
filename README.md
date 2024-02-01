4287d55b886122e3a9f99daea540c0874e56af45
mechabubba <stevenvanniisprettycool@gmail.com>
2024-01-31 21:41:36 -0600

    fix up latex command + clean up repo

--- README.md
+++ README.md
@@ -1,19 +1,20 @@
 <img align="right" src="assets/icon_static_small.png">
 
-# <a href="https://goon.haus/bear/">bear</a>
-<a href="https://discord.gg/9gdMpBR6bK">![Join the server!](https://img.shields.io/discord/525773944351883304?label=discord)</a> ![thanks mom](https://img.shields.io/badge/shoutouts%20to-my%20mom-ff69b4)
+# <a href="https://mechabubba.github.io/bear/">bear</a>
+<a href="https://discord.gg/9gdMpBR6bK">![Join the dev server!](https://img.shields.io/discord/525773944351883304?label=discord)</a> ![thanks mom](https://img.shields.io/badge/shoutouts%20to-my%20mom-ff69b4)
 
 *"the best discord bot"* — @everyone
 
-bear [stylized accordingly] is my Discord bot. Invite the bot [here](https://discord.com/oauth2/authorize?client_id=435224030459723776&scope=bot&permissions=8); this link will automatically give it the "`ADMINISTRATOR`" permission.
+bear [stylized accordingly] is my personal Discord bot. While it doesn't currently have a main focus or utility, it includes many features that I (mechabubba) find useful and use regularly. You can invite the official bot [here](https://discord.com/oauth2/authorize?client_id=435224030459723776&scope=bot&permissions=8); do note that this link will automatically give it the "`ADMINISTRATOR`" permission.
 
-bear is a modified fork of [sandplate](https://github.com/06000208/sandplate), a powerful template bot. See its readme for more information.
+bear is a modified fork of [sandplate](https://github.com/06000208/sandplate) 0.0.7, a powerful template bot. See its readme for more information.
 
 ## Installing
 1. `git clone https://github.com/mechabubba/bear.git`.
-2. Enter the folder and run `npm i`.
+2. Enter the folder and `npm i`.
 3. Install a bunch of shit;
 ```sh
+# for debian/raspbian users;
 $ sudo apt-get install fortune-mod cowsay latex
 ```
 4. Compile [`mathtex`](https://github.com/mechabubba/mathtex) (name the binary `mathtex`) and throw that in the bin folder.
@@ -21,11 +22,12 @@ $ sudo apt-get install fortune-mod cowsay latex
 ## Launching
 Running `node index.js` initially will create a config.json file in the data folder. Throw a bot token in there under `client.token` and now you're cooking with gas.
 
-## Contributing
-This is my bot, so every contribution is under heavy scrutiny because bear is my little scrunkly. But if you feel the need to add something or fix something, then please create a PR. Might get closed flat out, might not. Shits a gamble, man.
+Eventually, there will be a proper docker or systemd configuration to allow it to be persistent. For now, if it for some reason shuts down, perhaps check out [my script?](https://gist.github.com/mechabubba/e17397e487951358681103321f499bde) (ﾉ´• ω •`)ノ
 
-Feel free to join the Discord server up by the header to share your thoughts.
+## Contributing
+This is my bot, so every contribution is under heavy scrutiny because bear is my *little scrunkly.* But if you feel the need to add something or fix something, then please create a PR. Might get closed flat out, might not. Shits a gamble, man.
+
+Feel free to join the Discord server up by the header to share your thoughts if you feel particularly strong about something.
 
 ## License
 This is licensed under the Unlicense.
-
--- bot/commands/latex.js
+++ bot/commands/latex.js
@@ -1,26 +1,33 @@
 const CommandBlock = require("../../modules/CommandBlock");
 const { spawn, execSync } = require("child_process");
-const { createHash } = require("crypto");
+const { accessSync, constants } = require("fs");
 const { Util, MessageAttachment } = require("discord.js");
 const log = require("../../modules/log");
 
 module.exports = new CommandBlock({
     names: ["latex"],
-    description: "*Tries* to render a string of latex.\nNote that sometimes it will shit the bed without any discernable reason.\n\nThe renderer is from the standard texlive debian package, with images generated through a special binary based on [mathtex](https://github.com/mechabubba/mathtex) by Josh Forkosh Associates, Inc (modified by mechabubba).",
+    description: "*Tries* to render a string of latex. Note that sometimes it will shit the bed without any discernable reason.\n\nThe renderer is from the standard texlive debian package, with images generated through a special binary based on [mathtex](https://github.com/mechabubba/mathtex) by Josh Forkosh Associates, Inc (modified by mechabubba).\n\nThis project is a work in progress.",
     usage: "[LᴬTₑX]"
 }, function(client, message, content, args) {
     const opts = {
-        defaultPackages: ["amsmath", "amsfonts", "amssymb", "color"], // default packages (handled by mathtex)
+        defaultPackages: ["amsmath", "amsfonts", "amssymb", "color"], // Default packages (currently already handled by mathtex).
         newPackages: [],
     };
 
-    if (!content) {
-        return message.reply(`${client.reactions.negative.emote} You must input a piece of text to render.`);
+    // First, check if mathtex exists in the bin directory. If it doesn't, don't try to do anything.
+    try {
+        accessSync("./bin/mathtex", constants.R_OK | constants.X_OK);
+    } catch(e) {
+        return message.reply(`${client.reactions.negative.emote} mathtex is not installed or is otherwise inaccessible. :(`);
     }
 
-    // handle user provided packages
-    // these are placed in array which prefaces them before all other commands
-    // a future consideration should be the run order of each package, but this preserves the order in which they're typed
+    if (!content) {
+        return message.reply(`${client.reactions.negative.emote} You must input a piece of text to render!`);
+    }
+
+    // Handle user provided packages.
+    // These are placed in array which allows them to be prefaced before all other commands.
+    // A future consideration should be the run order of each package, but this preserves the order in which they're typed.
     const matches = content.matchAll(/\\usepackage{([a-z0-9-_]+)}/g);
     for (const match of matches) {
         const pkg = match[1];
@@ -35,37 +42,34 @@ module.exports = new CommandBlock({
 
     let eqn = `${opts.newPackages.map(x => `\\usepackage{${x}}`).join(" ")} \\color{white} ${content}`; 
     let buf = Buffer.from([]);
-    let err = ""; // error data
-    log.debug(`INPUT EQN: ${eqn}`);
+    let err = ""; // Variable to store stderr data.
 
     const child = spawn("./bin/mathtex", ["-m 0", "-d 240", "-s", "-t", eqn]);
     let threw = false;
 
     child.stdout.on("data", (d) => {
-        //log.debug(d);
         //log.debug(d.toString('utf8'));
         buf = Buffer.concat([buf, d])
     });
     child.stderr.on("data", (d) => {
         if (!d.toString().includes("locate")) {
-            threw = true; // temp
+            threw = true; // temp because of weird bug that might be fixed
         }
         err += d.toString();
     });
-    
     child.on("close", (code) => {
         if(threw) {
             return message.reply(`${client.reactions.negative.emote} An error occured;\`\`\`\n${err}\`\`\``);
         }
 
-        // mathtex supports png and gif output via a directive. check for either here.
+        // mathtex supports .png and .gif output via a directive. Check for either via magic bytes here.
         let ext = buf.subarray(0, 8).toString("latin1");
         if(ext.startsWith('\x89\x50\x4E\x47\x0D\x0A\x1A\x0A')) {
             ext = "png";
         } else if (ext.startsWith('\x47\x49\x46\x38')) {
             ext = "gif";
         } else {
-            // should never happen
+            // Should never happen...
             ext = undefined; 
         }
         const attachment = new MessageAttachment(buf, `out${ext ? `.${ext}` : ""}`);
@@ -73,6 +77,11 @@ module.exports = new CommandBlock({
     });
 });
 
+/**
+ * Helper function that searches for installed latex packages.
+ * @param {string} name The package to search for.
+ * @returns {boolean} Whether its installed or not.
+ */
 const packageinstalled = (name) => {
     try {
         execSync(`kpsewhich ${name}.sty`);
