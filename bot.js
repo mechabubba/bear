27dda9965fa10af86a9b77b043cca9d41bbda286
Amber G. <a06000208@protonmail.com>
2025-08-19 16:59:49 -0500

    Add discordjs v13 direct message intents

--- bot.js
+++ bot.js
@@ -6,7 +6,14 @@ const { token } = require("./modules/regexes");
 
 // Instantiate client
 const client = new Client({
-    intents: [Intents.FLAGS.GUILDS, Intents.FLAGS.GUILD_MEMBERS, Intents.FLAGS.GUILD_MESSAGES, Intents.FLAGS.GUILD_MESSAGE_REACTIONS],
+    intents: [
+        Intents.FLAGS.GUILDS,
+        Intents.FLAGS.GUILD_MEMBERS,
+        Intents.FLAGS.GUILD_MESSAGES,
+        Intents.FLAGS.GUILD_MESSAGE_REACTIONS,
+        Intents.FLAGS.DIRECT_MESSAGES,
+        Intents.FLAGS.DIRECT_MESSAGE_REACTIONS,
+    ],
 });
 
 // Token validation (config)
